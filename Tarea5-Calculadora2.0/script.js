c793f374a0514fff6a3ce51ff02cc0d1ba9271d1
Jerec4stro <jerec4stro_git@proton.me>
2022-11-19 13:05:38 -0300

    Calculadora con interfaz grafica

---
+++ Tarea5-Calculadora2.0/script.js
@@ -0,0 +1,26 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+      // 34567 numero
+      // 01234 indice
+    
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Syntax Error";
+      setTimeout(()=>display.innerText="", 2000);
+    
+    }else{
+        display.innerText += item.id
+    }
+  };
+});
